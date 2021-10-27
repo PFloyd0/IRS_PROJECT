8bb566d09811ee50f4bb4adfa5ec26897c9e99e4
lcw <1149117753@qq.com>
2021-10-27 18:47:06 +0800

    lcw version_1

--- book_re/login.py
+++ book_re/login.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import render
 from django.contrib.auth import authenticate,login
-
+from . import models
+from recommendation_system import rbm_cf_books_tf2
 
 def start(request):
     return render(request, 'login.html')
@@ -13,7 +14,15 @@ def my_login(request):
         user = authenticate(request, username=request.POST['email'], password=request.POST['password'])
         if user:
             login(request, user)
-            return render(request, 'index.html')
+            User_cast = models.User_cast.objects.get(user=user)
+            book_re = rbm_cf_books_tf2.do_recommendation(User_cast.cast_id)
+            book_name = book_re['Name_x']
+            print(book_name)
+            re = {}
+            for i in range(len(book_name)):
+                re["name%d"%i] = book_name.iloc[i]
+            print(re)
+            return render(request, 'index.html', re)
         else:
             message = '用户名或密码错误'
     return render(request, 'login.html', {'message': message})
--- book_re/register.py
+++ book_re/register.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-
+from . import models
 
 # 接收POST请求数据
 def register(request):
@@ -11,6 +11,8 @@ def register(request):
     except:
         if request.POST['password'] == request.POST['password_2'] and len(request.POST['password']) != 0:
             User.objects.create_user(request.POST['username'], request.POST['email'], request.POST['password'])
+            p = models.User_cast(user=User.objects.get(username=request.POST.get('username')))
+            p.save()
             info['lala'] = request.POST['username']
             return render(request, 'login.html', info)
         else:
